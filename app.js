a703d7f89b07a48803528ea4e2004696eff0e69d
Tharun <tharunprasad.k@gmail.com>
2025-06-12 23:24:34 +0530

    Added basic navbar

---
+++ app.js
@@ -0,0 +1,14 @@
+
+fetch('version.json')
+    .then(response => response.json())
+    .then(data => {
+      setTimeout(() => {
+        const versionDisplay = document.getElementById('version-number');
+        versionDisplay.textContent = `v${data.version}`;
+      }, 2000); // 2000ms = 2 seconds delay
+    })
+    .catch(error => {
+      console.error('Error loading version info:', error);
+    });
+
+
