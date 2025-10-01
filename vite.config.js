08b008a44cc4f9a41b1294717a50988fe75ed166
Tharun <tharunprasad.k@gmail.com>
2025-10-01 21:01:16 +0530

    config for web hosting

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  server: {
-    host: true,
-    port: 5173,
-    allowedHosts: true,
-  }
+  base: '/', 
 })
