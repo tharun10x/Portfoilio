bcec389c1702e8372543b0ba76870f06dfcebb05
Tharun Prasad K <tharunprasad.k@gmail.com>
2025-10-31 09:11:41 +0000

    checking the working state of the repository

--- src/App.jsx
+++ src/App.jsx
@@ -34,8 +34,6 @@ function App() {
       <section id="contact" className="min-h-[50vh] w-full static" bg- aria-label="Contact section">
       </section>
       <Footer className="absolute bottom-4 left-0 right-0" />
-      
-
     </>
     
   );
